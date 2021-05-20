c5401ea5d339b16237b8170a22d09b7be7ebc8d2
xiaozealer <xiaozealer@gmail.com>
2021-05-20 23:44:32 +0800

    add some test cases

--- autodiff.py
+++ autodiff.py
@@ -1,11 +1,13 @@
+from numpy import sin
 from Variable import *
-
+import numpy as np
 a = Variable(4, 'a')
 b = Variable(3, 'b')
-c = a - b
-d = a / c
-gradient = d.get_gradient()
-print(gradient[a])
-print(gradient[b])
-print(gradient[c])
-print(gradient[d])
+c = a*sin(b)
+gradient = c.get_gradient()
+assert gradient[a] == np.sin(b.val)
+assert gradient[b] == a.val*np.cos(b.val)
+
+# print(gradient[b])
+# print(gradient[c])
+# print(gradient[d])
