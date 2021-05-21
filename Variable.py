77c64fdf33ae82a41493bd715ac5646a3b94d0b6
xiaozealer <xiaozealer@gmail.com>
2021-05-21 18:20:51 +0800

    support new functions

--- Variable.py
+++ Variable.py
@@ -16,13 +16,11 @@ class Variable:
     def __add__(self, b):
         result = self.val + b.val
         gradient = ([self, 1], [b, 1])
-        print(gradient)
         return Variable(result, gradient=gradient)
 
     def __mul__(self, b):
         result = self.val * b.val
         gradient = ((self, b.val), (b, self.val))
-        print(gradient)
         return Variable(result, gradient=gradient)
 
     def __neg__(self):
@@ -33,28 +31,15 @@ class Variable:
     def __sub__(self, b):
         result = self.val - b.val
         gradient = ((self, 1), (b, -1))
-        print(gradient)
         return Variable(result, gradient=gradient)
 
     def __truediv__(self, b):
         result = self.val / b.val
         gradient = ((self, 1/b.val), (b, -self.val * (b.val**(-2))))
-        print(gradient)
         return Variable(result, gradient=gradient)
 
     def __repr__(self) -> str:
         return '{}:{}'.format(self.name, self.val)
-
-    # TODO add more activation functions
-    def sin(self):
-        result = np.sin(self.val)
-        gradient = ((self, np.cos(self.val)),)
-        return Variable(result, gradient=gradient)
-
-    def cos(self):
-        result = np.cos(self.val)
-        gradient = ((self, -np.sin(self.val)))
-        return Variable(result, gradient=gradient)
 
     def get_gradient(self):
         gradients = defaultdict(lambda: 0)
@@ -66,3 +51,29 @@ class Variable:
             local_grad = last_gradient * derative
             gradients[var] += local_grad
             var.compute_gradient(gradients, local_grad)
+
+ # TODO add more activation functions
+
+
+def sin(a):
+    result = np.sin(a.val)
+    gradient = ((a, np.cos(a.val)),)
+    return Variable(result, gradient=gradient)
+
+
+def cos(a):
+    result = np.cos(a.val)
+    gradient = ((a, -np.sin(a.val)),)
+    return Variable(result, gradient=gradient)
+
+
+def exp(a):
+    result = np.exp(a.val)
+    gradient = ((a, np.exp(a.val)),)
+    return Variable(result, gradient=gradient)
+
+
+def log(a):
+    result = np.log(a.val)
+    gradient = ((a, 1/a.val),)
+    return Variable(result, gradient=gradient)
